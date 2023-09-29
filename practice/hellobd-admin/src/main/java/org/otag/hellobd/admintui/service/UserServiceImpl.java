4c0cef3086e1f352513d03fa4359cc68dad8442e
0tak2 <0tak2.code@gmail.com>
2023-09-30 08:52:17 +0900

    시스템 관리자만 로그인할 수 있도록 수정

--- practice/hellobd-admin/src/main/java/org/otag/hellobd/admintui/service/UserServiceImpl.java
+++ practice/hellobd-admin/src/main/java/org/otag/hellobd/admintui/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package org.otag.hellobd.admintui.service;
 import lombok.AllArgsConstructor;
 import org.otag.hellobd.admintui.Global;
 import org.otag.hellobd.admintui.entity.User;
+import org.otag.hellobd.admintui.entity.enums.RoleEnum;
 import org.otag.hellobd.admintui.repository.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Component;
@@ -19,6 +20,11 @@ public class UserServiceImpl implements UserService {
 
         User user = repository.findByUsernameAndPassword(username, BCrypt.hashpw(password, salt))
                 .orElseThrow(() -> new RuntimeException("아이디나 비밀번호가 잘못되었습니다."));
+
+        if (user.getRole() != RoleEnum.SYS_ADMIN) {
+            throw new RuntimeException("시스템 관리자만 로그인할 수 있습니다.");
+        }
+
         global.setLoginedUser(user);
     }
 
